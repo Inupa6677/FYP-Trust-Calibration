5f202b580ca4fcec6c272fa239fa7ec277ee5fd3
Inupa6677 <135936854+Inupa6677@users.noreply.github.com>
2025-09-06 12:40:56 +0530

    using mistral modle for testing

--- llm-pddl/main.py
+++ llm-pddl/main.py
@@ -339,7 +339,7 @@ class Planner:
         print("[INFO] Using Ollama locally...")
         try:
             result = ollama.generate(
-                model="starcoder:7b",
+                model="mistral:latest",
                 prompt=prompt_text,
                 options={"temperature": 0.7, "top_p": 0.2, "num_ctx": 4096}
             )

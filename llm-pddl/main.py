e82e0ca484bc307176098fbcec15f65c0bfe9304
Inupa6677 <135936854+Inupa6677@users.noreply.github.com>
2026-01-08 09:09:20 +0530

    new mostral for t 0.5

--- llm-pddl/main.py
+++ llm-pddl/main.py
@@ -355,9 +355,9 @@ class Planner:
         print("[INFO] Using Ollama locally...")
         try:
             result = ollama.generate(
-                model="qwen2.5:7b-instruct",
+                model="mistral:latest",
                 prompt=prompt_text,
-                options={"temperature": 0.4, "top_p": 0.2, "num_ctx": 4096}
+                options={"temperature": 0.5, "top_p": 0.2, "num_ctx": 4096}
             )
             return result['response'].strip()
         except Exception as e:
